05b2cc506faa90029ed05adf9fbbc60543b2c3fc
Christian Buela <20210022296@my.xu.edu.ph>
2023-11-17 14:34:13 +0800

    3rd commit

--- main.py
+++ main.py
@@ -14,5 +14,25 @@ def GetProduct(ProductId):
     else:
         return jsonify({"message": "Product not found"}), 404
 
+@app.route('/product', methods=['POST'])
+def AddProduct():
+    data = request.get_json()
+
+    RequiredFields = ['productName', 'category', 'price']
+    if not all(field in data for field in RequiredFields):
+        return jsonify({"message": "Missing required fields"}), 400
+
+    NewId = max([p['id'] for p in products]) + 1 if products else 1
+
+    NewProduct = {
+        "id": NewId,
+        "productName": data['productName'],
+        "category": data['category'],
+        "price": data['price'],
+    }
+
+    products.append(NewProduct)
+
+    return jsonify({"message": "Product added successfully", "id": NewId}), 201
 
 
